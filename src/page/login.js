8c4b91db94b9260cf5d457bc233dadf6b7f8fa59
Akash Dhiman <akashdhiman@Akashs-MacBook-Air.local>
2024-07-06 12:33:10 +0530

    errors resolved

--- src/page/login.js
+++ src/page/login.js
@@ -15,7 +15,9 @@ function Login(){
     const handleSignup = async () =>{
         supabase.auth.signInWithOAuth({
             provider: 'google',
-            
+            options:{
+                redirectTo: 'https://travelbudy.netlify.app/main',
+            }
           })
           
     }
--- src/page/signup.js
+++ src/page/signup.js
@@ -15,6 +15,9 @@ function Signup() {
     const handleSignup = async () => {
         supabase.auth.signInWithOAuth({
             provider: 'google',
+            options:{
+                redirectTo: 'https://travelbudy.netlify.app/main',
+            }
             
         })
 
