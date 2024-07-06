8f005dc5703cfac164f5d676e886abb20bee08ef
Akash Dhiman <akashdhiman@Akashs-MacBook-Air.local>
2024-07-06 13:39:25 +0530

    errors resolved

--- src/hooks/useChat.js
+++ src/hooks/useChat.js
@@ -1,13 +1,13 @@
 
 import axios from "axios"
 export async function getAllChatRooms(values) {
-    const data = await axios.get('https://https://travelbuddy-backend-gxl9.onrender.com/chat/rooms');
+    const data = await axios.get('https://travelbuddy-backend-gxl9.onrender.com/chat/rooms');
     console.log(data)
     return data.data;
 }
 
 export async function createChat(values) {
-    const data = await axios.post('https://https://travelbuddy-backend-gxl9.onrender.com/chat', {
+    const data = await axios.post('https://travelbuddy-backend-gxl9.onrender.com/chat', {
         users: ["6687b8052be27e93d9938d4e", values]
     });
     console.log(data)
