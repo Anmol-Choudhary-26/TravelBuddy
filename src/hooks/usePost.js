1459a0b571a5a305b32fdf4f4ca85a7c720c5136
Akash Dhiman <akashdhiman@Akashs-MacBook-Air.local>
2024-07-12 12:17:03 +0530

    backend

--- src/hooks/usePost.js
+++ src/hooks/usePost.js
@@ -1,22 +1,20 @@
 import axios from "axios"
-import moment from "moment";
 export async function getAllPost(values) {
-    const data = await axios.get('https://travelbuddy-backend-gxl9.onrender.com/post/allpost');
+    const data = await axios.get(`https://cors-anywhere.herokuapp.com/https://travelbuddy-backend-gxl9.onrender.com/post/allpost`, 
+        {headers: {
+            "X-Requested-With": "XMLHttpRequest"
+          },} 
+    )
     console.log(data)
-    return data.data;
+    return data.data
 }
 
 export async function createPost(values) {
-    const formattedDate = moment().toISOString(values.date);
-    const data = await axios.post('https://travelbuddy-backend-gxl9.onrender.com/post', {
-        imageUrl: values.imageUrl,
-        authorName: values.authorName,
-        authorId: values.authorId,
-        location: values.location,
-        caption: values.caption,
-        date: formattedDate,
-        
-    });
+    const data = await axios.post(`https://cors-anywhere.herokuapp.com/https://travelbuddy-backend-gxl9.onrender.com/post`, values,
+        {headers: {
+            "X-Requested-With": "XMLHttpRequest"
+          },}  
+    )
     console.log(data)
     return data.data
 }
--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 export async function createUser(values) {
-    const data = await axios.post('https://travelbuddy-backend-gxl9.onrender.com/user', 
+    const data = await axios.post('https://cors-anywhere.herokuapp.com/https://travelbuddy-backend-gxl9.onrender.com/user', 
      {
         email: values.email,
         phoneNumber: values.phoneNumber,
@@ -8,13 +8,21 @@ export async function createUser(values) {
         UserName: values.userName,
         shortBio: values.shortBio,
         Address: values.address
-    })
+    }, 
+    {headers: {
+        "X-Requested-With": "XMLHttpRequest"
+      },} 
+)
     console.log(data)
     return data
 }
 
 export async function updateUser(values) {
-    const data = await axios.put(`https://travelbuddy-backend-gxl9.onrender.com/user/${"6687b8052be27e93d9938d4e"}`, values)
+    const data = await axios.put(`https://cors-anywhere.herokuapp.com/https://travelbuddy-backend-gxl9.onrender.com/user/${"6687b8052be27e93d9938d4e"}`, values,
+        {headers: {
+            "X-Requested-With": "XMLHttpRequest"
+          }, }
+    )
     console.log(data)
     return data
 }
